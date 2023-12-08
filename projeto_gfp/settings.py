5df44974292809432896d06238f170d84755fe42
Dusax <lucas.emmanuel@ufrpe.br>
2023-12-08 18:50:19 -0300

    Inseri os dados para conexão com o banco de dados local que havia criado, já alterei e agora está permitido o acesso remoto ao banco de dados.

--- projeto_gfp/settings.py
+++ projeto_gfp/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'projeto_gfp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "NAME": "edsgfp",
+        "USER": "admeds",
+        "PASSWORD": "Secr!t@@",
+        "HOST": "192.168.0.101",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
