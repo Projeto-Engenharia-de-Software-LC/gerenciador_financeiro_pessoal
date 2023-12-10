ea2e56aaa52c817697d9b5791e1f6d048ff49430
Dusax <lucas.emmanuel@ufrpe.br>
2023-12-10 16:15:53 -0300

    Adicionado as bibliotecas relacionadas ao MySQL no arquivo requirements.txt

--- projeto_gfp/settings.py
+++ projeto_gfp/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'projeto_gfp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'edsgfp',
+        'USER': 'admeds',
+        'PASSWORD': 'Secr!t@@',
+        'HOST': '192.168.0.101',
+        'PORT': '3306',
     }
 }
 
