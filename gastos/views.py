d2cfc5413b87d17d88d0c6f2c2b0efbe7755188a
unknown <walquiriawps@gmail.com>
2023-12-17 21:26:55 -0300

    criação de uma tela home, update e delete

--- gastos/views.py
+++ gastos/views.py
@@ -3,6 +3,24 @@ from django.shortcuts import render, redirect
 from .forms import GastoForm
 from .models import Gasto
 
+def home(request):
+    return render(request,'home.html')
+
+def atualizar_gasto(self,request, pk):
+    print("aqui");
+    gasto = Gasto.objects.get(pk=pk)
+    form = GastoForm(request.POST or None, instance=gasto)
+    if form.is_valid():
+        form.save()
+        return redirect('listar_gastos')
+    return render(request, 'cadastrar_gasto.html', {'form': form})
+
+def remover_gasto(request, pk):
+    gasto = get_object_or_404(Gasto, pk=pk)
+    if request.method == 'POST':
+        gasto.delete()
+        return redirect('listar_gastos')
+    return render(request, 'confirmar_remocao.html', {'gasto': gasto})
 
 def listar_gastos(request):
     gastos = Gasto.objects.all()
