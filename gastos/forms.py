59a2c8fb0c68d52d0d995fb5b92814a3dae32f06
Vinicius Felipe <viniciusfelipe.xd@gmail.com>
2024-02-18 17:35:57 -0300

    improvement: add css attributes in login/register form field

--- gastos/forms.py
+++ gastos/forms.py
@@ -4,15 +4,27 @@ from django import forms
 from .models import Gasto, Receita, Usuario
 from django.contrib.auth.forms import AuthenticationForm
 
+
 class LoginForm(AuthenticationForm):
     class Meta:
         model = Usuario
         fields = ['username', 'password']
+        
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class' : 'form-control'})
+        self.fields['password'].widget.attrs.update({'class' : 'form-control'})
 
 class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = ['username','email','password']
+        
+    def __init__(self, *args, **kwargs):
+        super(UsuarioForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class' : 'form-control'})
+        self.fields['password'].widget.attrs.update({'class' : 'form-control'})
+        self.fields['email'].widget.attrs.update({'class' : 'form-control'})
 
 class ReceitaForm(forms.ModelForm):
     class Meta:
